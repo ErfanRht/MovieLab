4b000c38c163b255a8a7a9ceac2f0c1a9ece8a0b
Erfan Rahmati <ErfanRht1384.com@gmail.com>
2022-05-18 15:46:31 +0430

    Add another connection error handler for the search page

--- lib/pages/main/search/search.dart
+++ lib/pages/main/search/search.dart
@@ -3,15 +3,19 @@ import 'package:movielab/constants/types.dart';
 import 'package:movielab/modules/api_requester.dart';
 import 'package:movielab/pages/main/search/search_bar/search_bar_controller.dart';
 
-doSearch() {
+doSearch() async {
   final apiRequester = APIRequester();
   final searchBarController = Get.find<SearchBarController>();
   searchBarController.setLoadingStatus(status: RequestResult.LOADING);
-  apiRequester.search().then((value) => {
-        print(value),
-        if (value)
-          searchBarController.setLoadingStatus(status: RequestResult.SUCCESS)
-        else
-          searchBarController.setLoadingStatus(status: RequestResult.FAILURE)
-      });
+  try {
+    await apiRequester.search().then((value) => {
+          if (value)
+            searchBarController.setLoadingStatus(status: RequestResult.SUCCESS)
+          else
+            searchBarController.setLoadingStatus(status: RequestResult.FAILURE)
+        });
+  } catch (e) {
+    await Future.delayed(const Duration(seconds: 1));
+    searchBarController.setLoadingStatus(status: RequestResult.FAILURE);
+  }
 }
