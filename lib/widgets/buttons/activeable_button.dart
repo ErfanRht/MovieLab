099b6903963592f8368fd7565d15c68fc1c4ca64
Erfan Rahmati <ErfanRht1384.com@gmail.com>
2022-08-12 12:31:55 +0430

    fix: Bug in show_popup_action

--- lib/widgets/buttons/activeable_button.dart
+++ lib/widgets/buttons/activeable_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class ActiveableButton extends StatelessWidget {
   final String text;
@@ -10,7 +11,7 @@ class ActiveableButton extends StatelessWidget {
   final Color backgroundColor;
   final Color? activeColor;
   final EdgeInsets margin;
-  const ActiveableButton(
+  ActiveableButton(
       {Key? key,
       required this.isActive,
       required this.text,
@@ -22,46 +23,58 @@ class ActiveableButton extends StatelessWidget {
       this.margin = const EdgeInsets.symmetric(vertical: 7.5),
       this.backgroundColor = const Color(0xff2a425f)})
       : super(key: key);
-
+  bool tapped = false;
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      height: 50,
-      margin: margin,
-      width: MediaQuery.of(context).size.width - 100,
-      decoration: BoxDecoration(
-          color: isActive
-              ? activeColor?.withOpacity(0.75) ??
-                  backgroundColor.withOpacity(0.75)
-              : backgroundColor.withOpacity(0.75),
-          borderRadius: BorderRadius.circular(15)),
-      duration: const Duration(milliseconds: 150),
-      child: TextButton(
-          onPressed: onTap,
-          style: TextButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15),
-            ),
-          ),
-          child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: Icon(
-                  isActive ? activeIcon ?? icon : icon,
-                  color: Colors.white,
-                  size: 20,
+    return StatefulBuilder(builder: (context, setState) {
+      return AnimatedContainer(
+        height: 50,
+        margin: margin,
+        width: MediaQuery.of(context).size.width - 100,
+        decoration: BoxDecoration(
+            color: isActive
+                ? activeColor?.withOpacity(0.75) ??
+                    backgroundColor.withOpacity(0.75)
+                : backgroundColor.withOpacity(0.75),
+            borderRadius: BorderRadius.circular(15)),
+        duration: const Duration(milliseconds: 150),
+        child: !tapped
+            ? TextButton(
+                onPressed: () {
+                  setState(() {
+                    tapped = true;
+                  });
+                  onTap();
+                },
+                style: TextButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
                 ),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
+                      child: Icon(
+                        isActive ? activeIcon ?? icon : icon,
+                        color: Colors.white,
+                        size: 20,
+                      ),
+                    ),
+                    Text(
+                      isActive ? activeText ?? text : text,
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w800),
+                    ),
+                  ],
+                ))
+            : const SpinKitThreeBounce(
+                color: Colors.white,
+                size: 20.0,
               ),
-              Text(
-                isActive ? activeText ?? text : text,
-                style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 15,
-                    fontWeight: FontWeight.w800),
-              ),
-            ],
-          )),
-    );
+      );
+    });
   }
 }
