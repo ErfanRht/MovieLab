29464fef238892462f231eec9eb9769cf4106a8c
Erfan Rahmati <ErfanRht1384.com@gmail.com>
2022-09-02 10:50:55 +0430

    improve: change main bottomNavigationBar widget

--- lib/modules/tools/system_ui_overlay_style.dart
+++ lib/modules/tools/system_ui_overlay_style.dart
@@ -5,7 +5,7 @@ import 'package:movielab/constants/types.dart';
 
 setSystemUIOverlayStyle(
     {required SystemUIOverlayStyle systemUIOverlayStyle,
-    Color color = kBackgroundColor}) {
+    Color color = kSecondaryColor}) {
   if (systemUIOverlayStyle == SystemUIOverlayStyle.DARK) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
         statusBarColor: Colors.transparent,
